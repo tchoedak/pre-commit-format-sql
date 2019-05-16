df6f3be7cbd2ebc26118b901bbb2a56bb07b4923
tchoedak <tchoedak@gmail.com>
2019-05-15 22:18:22 -0700

    bumpv

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='pre-commit-format-sql',
     description='a pre-commit hook for formatting sql',
-    version='0.0.1',
+    version='0.0.2',
     license='BSD',
     author='tchoedak',
     install_requires=[
