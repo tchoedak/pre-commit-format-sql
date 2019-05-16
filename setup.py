4363d624446dd8af8928301d30c5e401fd6c2b71
tchoedak <tchoedak@gmail.com>
2019-05-16 15:49:03 -0700

    restructure as a mirror

--- setup.py
+++ setup.py
@@ -4,13 +4,8 @@ from setuptools import setup
 setup(
     name='pre-commit-format-sql',
     description='a pre-commit hook for formatting sql',
-    version='0.0.2',
-    license='BSD',
-    author='tchoedak',
+    version='0.0.3',
     install_requires=[
         'format-sql'
     ],
-    entry_points={
-        'console_scripts': ['format=format:main']
-    }
 )
